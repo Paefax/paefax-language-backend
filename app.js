334e6bf5f097b20a79bd5d063c50c4ee84aad567
HelenaHalldinITHS <helena.halldin@iths.se>
2022-10-11 15:47:30 +0200

    Add a draft for creating quiz

--- app.js
+++ app.js
@@ -10,3 +10,49 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const getQuiz = (rows, currentCategory) => {
+  var currentQuestions = [];
+  console.log("rows", rows);
+
+  currentQuestions.push({
+    id: 0,
+    word: rows[0].english,
+    correctAnswer: rows[0].swedish,
+    incorrectAnswers: ["ananas", "kiwi"],
+  });
+  currentQuestions.push({
+    id: 1,
+    word: rows[1].english,
+    correctAnswer: rows[1].swedish,
+    incorrectAnswers: ["äpple", "mango"],
+  });
+
+  return {
+    category: currentCategory,
+    questions: currentQuestions,
+  };
+
+  /*
+  rows = [
+    { english: 'apple', swedish: 'äpple' },
+    { english: 'apple', swedish: 'äpple' },
+    { english: 'apple', swedish: 'äpple' }
+  ]*/
+};
+
+app.get("/fruit", (req, res) => {
+  var sql = "select english,swedish from fruit order by random() limit 15;";
+  var params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    var quizObject = getQuiz(rows, "fruit");
+    res.json({
+      message: "success",
+      quiz: quizObject,
+    });
+  });
+});
