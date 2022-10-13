bda25af52e2e4e1fbf9a9ef8745020af5feaa1ec
HelenaHalldinITHS <helena.halldin@iths.se>
2022-10-13 11:19:24 +0200

    Implement endpoint for animal

--- routes/animal.js
+++ routes/animal.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const router = express.Router();
 let animalDB = require("../animalDatabase.js");
+const utilities = require("./utilities");
+const numberOfQuestions = 1;
+const implementedLanguages = ["swedish"];
 
-router.get("/", (req, res) => {
-  res.json({ message: "Hello from animal" });
+router.get("/:language", (req, res) => {
+  if (!implementedLanguages.includes(req.params.language)) {
+    console.log("not included");
+    res.status(400).json({
+      error: `${req.params.language} is not an implemented language. Implemented languages are ${implementedLanguages}`,
+    });
+    return;
+  }
+  let sql = `select english,${req.params.language} from animal order by random() limit ?;`;
+  let params = [numberOfQuestions * 3];
+  animalDB.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    let currentQuestions = utilities.getQuestions(rows, numberOfQuestions);
+    res.json({
+      category: "animal",
+      questions: currentQuestions,
+    });
+  });
 });
 
 module.exports = router;
