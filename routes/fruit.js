47b1c7c3bfffec31f098f2149dcc50b33535febd
HelenaHalldinITHS <helena.halldin@iths.se>
2022-10-13 13:50:27 +0200

    move utilities

--- routes/fruit.js
+++ routes/fruit.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 let fruitDB = require("../fruitDatabase.js");
-const utilities = require("./utilities");
+const utilities = require("./utilities/utilities");
 const numberOfQuestions = 5;
 const implementedLanguages = ["swedish"];
 
