d57e6f4cec52482e3ca9723fa04952097d6a940b
HelenaHalldinITHS <helena.halldin@iths.se>
2022-10-31 13:16:05 +0100

    remove ".../" endpoint

--- app.js
+++ app.js
@@ -22,38 +22,6 @@ app.use("/languages", languages);
 app.use("/categories", categories);
 app.use("/progress", progress);
 
-app.get("/", (req, res) => {
-  res.json({
-    category: "Fruit",
-    questions: [
-      {
-        id: 0,
-        word: "apple",
-        correctAnswer: "äpple",
-        incorrectAnswers: ["ananas", "kiwi"],
-      },
-      {
-        id: 1,
-        word: "pear",
-        correctAnswer: "päron",
-        incorrectAnswers: ["äpple", "mango"],
-      },
-      {
-        id: 2,
-        word: "pineapple",
-        correctAnswer: "ananas",
-        incorrectAnswers: ["mango", "jordgubbe"],
-      },
-      {
-        id: 3,
-        word: "strawberry",
-        correctAnswer: "jordgubbe",
-        incorrectAnswers: ["björnbär", "hallon"],
-      },
-    ],
-  });
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
