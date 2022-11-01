d5eeb0cc2bceab2fd4c9d66a201590ce731f14c6
FelixJacobsen <felix.jacobsen@iths.se>
2022-11-01 14:58:07 +0100

    Add expiresIn again

--- routes/user.js
+++ routes/user.js
@@ -76,7 +76,7 @@ router.post("/login", async (req, res) => {
   const username = req.body.username;
   const user = { name: username };
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "900s",
+    expiresIn: "800s",
   });
 
   rowData = [];
